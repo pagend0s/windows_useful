939f306ee1e4b343d6e0975a2b63105c9505ce83
pagend0s <65870887+pagend0s@users.noreply.github.com>
2021-11-03 14:29:38 +0100

    Add files via upload

---
+++ PL/restrict_taskmgr.bat
@@ -0,0 +1,38 @@
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+GOTO MAIN
+
+:MAIN
+
+wmic UserAccount get Name
+	set "user_id="
+	set /p "user_id=PODAJ NAZWE UZYTKOWNIKA DLA KTOREGO CHCESZ ZABLOKOWAC DOSTEP DO TASKMGR: "
+
+reg load HKLM\TempHive C:\Users\%user_id%\ntuser.dat
+
+REG.EXE ADD HKLM\TempHive\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\	/v DisableTaskmgr /t REG_DWORD /d 1 /f
+
+pause
